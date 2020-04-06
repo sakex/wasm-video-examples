3985a87aa6cf56263b68ababe4624d226a0fa6d3
sakex <alexandre@senges.ch>
2020-04-06 17:04:56 +0200

    No Errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,11 +27,12 @@ export default class Index extends Component<{}, State> {
         this.socket.on("call", async ({id, selfId, data}: CallParams) => {
             const offer = JSON.parse(data);
             this.streaming = new Streaming(document.querySelector("#firstVideo"));
+
+            this.socket.on("candidate", ({id, candidate}) => this.streaming.add_ice_candidate(candidate));
+            const answer = await this.streaming.accept_offer(offer).get_offer();
             this.streaming.set_on_ice_candidate((candidate) => {
                 this.socket.emit("candidate", {candidate: candidate, id: selfId});
             });
-            this.socket.on("candidate", ({id, candidate}) => this.streaming.add_ice_candidate(candidate));
-            const answer = await this.streaming.accept_offer(offer).get_offer();
             await this.streaming.load_video();
             this.socket.emit("answer", ({id: selfId, selfId: this.state.conId, data: JSON.stringify(answer)}));
         });
