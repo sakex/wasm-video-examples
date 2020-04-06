9813aeb7ebbb22a96d81c6cc3c5b9fc4b54d871a
sakex <alexandre@senges.ch>
2020-04-06 14:59:40 +0200

    IceCandidate

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,14 +28,13 @@ export default class Index extends Component<{}, State> {
             const offer = JSON.parse(data);
             this.streaming = new Streaming(document.querySelector("#firstVideo"));
             this.streaming.set_on_ice_candidate((candidate) => {
-                this.socket.emit("candidate", {candidate, id: selfId});
-                console.log(candidate);
+                this.socket.emit("candidate", {candidate: JSON.stringify(candidate), id: selfId});
             });
+            this.socket.on("candidate", ({id, candidate}) => this.streaming.add_ice_candidate(JSON.parse(candidate)));
             const answer = await this.streaming.accept_offer(offer).get_offer();
             await this.streaming.load_video();
             this.socket.emit("answer", ({id: selfId, selfId: this.state.conId, data: JSON.stringify(answer)}));
         });
-        this.socket.on("candidate", ({id, candidate}) => this.streaming.add_candidate(candidate));
         this.socket.on("answer", async ({id, selfId, data}: CallParams) => {
             const offer = JSON.parse(data);
             await this.streaming.accept_answer(offer).get_offer();
@@ -50,9 +49,9 @@ export default class Index extends Component<{}, State> {
     callRemote = async (user: number) => {
         this.streaming = new Index.Streaming(document.querySelector("#firstVideo"));
         this.streaming.set_on_ice_candidate((candidate) => {
-            console.log("in call")
-            this.socket.emit("candidate", {candidate, id: user});
+            this.socket.emit("candidate", {candidate: JSON.stringify(candidate), id: user});
         });
+        this.socket.on("candidate", ({id, candidate}) => this.streaming.add_ice_candidate(JSON.parse(candidate)));
         const offer = await this.streaming.create_offer().get_offer();
         this.socket.emit("call", ({id: user, selfId: this.state.conId, data: JSON.stringify(offer)}));
     };
