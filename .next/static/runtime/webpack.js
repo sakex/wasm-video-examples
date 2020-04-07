d8e56c92c692dd19c286a617ea0f528401617dd3
r0778961 <olivierharaldjoachim.charrez@student.kuleuven.be>
2020-04-07 22:31:08 +0200

    table

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2ed770ea99b2e61c083e";
+/******/ 	var hotCurrentHash = "daa8fdc1e30bdcdcdfe3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- ws/poker.js
+++ ws/poker.js
@@ -8,39 +8,23 @@ class Poker {
         this.id = id;
         this.feedSocket();
     }
-    
+
     feedSocket = () => {
 
         this.socket.on("join", () => {
             console.log(" Your id is: ", this.id);
             console.log(" Size is: ", Poker.players.size);
 
-            for (let player of Poker.players) {
-                console.log("player is: ", player);
-                if (player === this.id) {
-                    console.log("already in");
-                } else {
-                    if (Poker.players.size < Poker.maxPlayer) {
-
-                        console.log("welcome to the table");
-                        Poker.players.add(this.id);
-                        console.log(Poker.players);
-                    } else {
-                        console.log("table already full");
-                    }
-                }
+            if (!Poker.players.has(this) && Poker.players.size < Poker.maxPlayer) {
+                Poker.players.add(this);
             }
-
-
         })
             .on("leave", () => {
-                console.log("sad to see you leave");
-                Poker.players.delete(this.id);
-                console.log(Poker.players);
+                Poker.players.delete(this);
             });
     };
 }
-
+//Poker.players.forEach(player => console.log(player.id));
 /*
 emitToAll = () => {
     this.players.forEach(player => player.socket.emit("msg", args);
